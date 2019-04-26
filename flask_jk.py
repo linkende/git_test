496499d2df2ca3fccf2ed2e4f68e1c9d935a31c4
linkende <linicenok@163.com>
2019-04-26 14:36:31 +0800

    3

--- flask_jk.py
+++ flask_jk.py
@@ -7,20 +7,20 @@ app.config['JSON_AS_ASCII'] = False
 time=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
 json_test={
     "date": time,
-    "uid": "20180626135827-3e39eedfa8ac4cdc8bceec7d0577f1b9",
-    "errorCode": 200,
-    "errorMsg": "请求成功",
+    "uid": "3e39eedfa8ac4cdc8bceec7d0577f1b9",
+    "Code": 200,
+    "Msg": "请求成功",
     "data": [{
-        "record": [{
+        "red": [{
             "realName": "周*",
-            "bankCard": "622848*********7618",
-            "resDesc": "认证通过",
-            "idCard": "513124********3415",
-            "mobile": "181****7305",
-            "resCode": "00"
+            "Card": "622848",
+            "res": "通过",
+            "id": "513124",
+            "ok": "7305",
+            "Code": "00"
         }],
-        "name": "银行卡精确四要素验证",
-        "recordNum": 1
+        "name": "验证",
+        "Num": 1
     }]
 
 }
